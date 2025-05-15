b7e0d15925f30be0293a63638e4ffd0d6fc31dbe
SigurtO <sigurt.olsen@hotmail.com>
2025-05-15 15:19:45 +0200

    Api stuff weird

--- BlazorProject/Program.cs
+++ BlazorProject/Program.cs
@@ -1,4 +1,6 @@
 using BlazorProject;
+using BlazorProject.Database;
+using BlazorProject.Tables;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +8,31 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
+using (var db = new BlazorDbContext())
+{
+    db.Database.EnsureCreated(); // Creates the database if it doesn't exist
+}
+
+using (BlazorDbContext db = new BlazorDbContext()) // prop shoudlnt do this is program.cs :)
+{
+    Employe employee = new Employe
+    {
+        Name = "John Doe",
+        JobTitle = "Delivery"
+    };
+	try
+	{
+		db.Employees.Add(employee);
+		db.SaveChanges();
+    }
+	catch (Exception)
+	{
+
+		throw;
+	}
+}
---
+++ BlazorProject/Tables/Employe.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorProject.Tables
+{
+    public class Employe
+    {
+        [Key] // auto increments like IDENTITY in MS SQL
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string JobTitle { get; set; }
+    }
+}
