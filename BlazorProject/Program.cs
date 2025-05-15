21dcf22d2f99e6aa432403c4c979ce2fb7097fb3
SigurtO <sigurt.olsen@hotmail.com>
2025-05-15 23:26:52 +0200

    Revert "Api stuff weird"
    This reverts commit b7e0d15925f30be0293a63638e4ffd0d6fc31dbe.

--- BlazorProject/Program.cs
+++ BlazorProject/Program.cs
@@ -1,6 +1,4 @@
 using BlazorProject;
-using BlazorProject.Database;
-using BlazorProject.Tables;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -8,31 +6,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-
-
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
-using (var db = new BlazorDbContext())
-{
-    db.Database.EnsureCreated(); // Creates the database if it doesn't exist
-}
-
-using (BlazorDbContext db = new BlazorDbContext()) // prop shoudlnt do this is program.cs :)
-{
-    Employe employee = new Employe
-    {
-        Name = "John Doe",
-        JobTitle = "Delivery"
-    };
-	try
-	{
-		db.Employees.Add(employee);
-		db.SaveChanges();
-    }
-	catch (Exception)
-	{
-
-		throw;
-	}
-}
